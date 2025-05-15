d1352de4dc6d36a5273e6db598832b0d6927d60d
chirupaleger123 <chiranjeevichiruc320@gmail.com>
2025-05-14 20:57:14 -0700

    Create arr_addition.c

---
+++ arr_addition.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+int i,arr[10],sum=0;
+printf("Enter the elements\n");
+for(i=0;i<10;i++){
+    scanf("%d",&arr[i]);
+}
+for(i=0;i<10;i++){
+    sum+=arr[i];
+}
+printf("%d",sum);
+return 0;
+}
